b7b4143b07f26bca2393203355810300c052b35f
Ophiran <ophiran@hotmail.com>
2013-11-20 01:31:37 +0100

    =

--- DAL-Library/dbAccess.cs
+++ DAL-Library/dbAccess.cs
@@ -10,6 +10,8 @@ namespace DAL_Library
     public class DbAccess
     {
         public DataClasses1DataContext dc = new DataClasses1DataContext();
+
+        #region News
         public List<New> getNews()
         {
             //DataClasses1DataContext dc = new DataClasses1DataContext();
@@ -48,5 +50,166 @@ namespace DAL_Library
             dc.SubmitChanges();
         }
 
+        #endregion
+
+        #region Articles
+        public List<Article> getArticles()
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            List<Article> articlesList = dc.Articles.ToList<Article>();
+            return articlesList;
+        }
+
+        public int getMaxIdArticles()
+        {
+            return dc.Articles.Max(n => n.Id);
+        }
+
+        public void addArticles(Article articles)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (!dc.Articles.Contains(articles))
+            {
+                articles.Id = getMaxIdArticles() + 1;
+                dc.Articles.InsertOnSubmit(articles);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void delArticles(Article articles)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (dc.Articles.Contains(articles))
+            {
+                dc.Articles.DeleteOnSubmit(articles);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void updArticles(Article articles)
+        {
+            dc.SubmitChanges();
+        }
+        #endregion
+
+        #region Rights
+        public List<Right> getRights()
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            List<Right> rightsList = dc.Rights.ToList<Right>();
+            return rightsList;
+        }
+
+        public int getMaxIdRights()
+        {
+            return dc.Rights.Max(n => n.Id);
+        }
+
+        public void addRights(Right rights)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (!dc.Rights.Contains(rights))
+            {
+                rights.Id = getMaxIdRights() + 1;
+                dc.Rights.InsertOnSubmit(rights);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void delRights(Right rights)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (dc.Rights.Contains(rights))
+            {
+                dc.Rights.DeleteOnSubmit(rights);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void updRights(Right rights)
+        {
+            dc.SubmitChanges();
+        }
+        #endregion
+
+        #region Types
+        public List<Type> getTypes()
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            List<Type> typesList = dc.Types.ToList<Type>();
+            return typesList;
+        }
+
+        public int getMaxIdTypes()
+        {
+            return dc.Types.Max(n => n.Id);
+        }
+
+        public void addTypes(Type types)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (!dc.Types.Contains(types))
+            {
+                types.Id = getMaxIdTypes() + 1;
+                dc.Types.InsertOnSubmit(types);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void delTypes(Type types)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (dc.Types.Contains(types))
+            {
+                dc.Types.DeleteOnSubmit(types);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void updTypes(Type types)
+        {
+            dc.SubmitChanges();
+        }
+        #endregion
+
+        #region Users
+        public List<User> getUsers()
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            List<User> usersList = dc.Users.ToList<User>();
+            return usersList;
+        }
+
+        public int getMaxIdUsers()
+        {
+            return dc.Users.Max(n => n.Id);
+        }
+
+        public void addUsers(User users)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (!dc.Users.Contains(users))
+            {
+                users.Id = getMaxIdTypes() + 1;
+                dc.Users.InsertOnSubmit(users);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void delUsers(User users)
+        {
+            //DataClasses1DataContext dc = new DataClasses1DataContext();
+            if (dc.Users.Contains(users))
+            {
+                dc.Users.DeleteOnSubmit(users);
+                dc.SubmitChanges();
+            }
+        }
+
+        public void updUsers(User users)
+        {
+            dc.SubmitChanges();
+        }
+        #endregion
     }
 }
