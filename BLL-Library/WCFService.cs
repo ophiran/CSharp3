d4234da6f5a352e0e2f896546aa81720098f5a2d
ophiran <ophiran@hotmail.com>
2013-11-26 22:51:39 +0100

    Added BLLlib + WPFClient

---
+++ BLL-Library/WCFService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BLL_Library
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFService" in both code and config file together.
+    public class WCFService : IWCFService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
