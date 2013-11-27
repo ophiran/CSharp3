1252be76f442021332ec9111e99704f3c12e0eee
Ophiran <ophiran@hotmail.com>
2013-11-27 09:38:25 +0100

    -

---
+++ WindowsService/NewsService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public partial class NewsWindowsService : ServiceBase
+    {
+        public NewsWindowsService()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("SystemLog"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "SystemLog", "LogEntry");
+            }
+            systemLog.Source = "SystemLog";
+            systemLog.Log = "LogEntry";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            systemLog.WriteEntry("-Service Start-");
+        }
+
+        protected override void OnStop()
+        {
+            systemLog.WriteEntry("-Service Stop-");
+        }
+
+        private void eventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
+        {
+
+        }
+    }
+}
