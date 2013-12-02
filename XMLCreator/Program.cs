365623161620df9fa2e6bfcf8069ad9df7ce1977
ophiran <ophiran@hotmail.com>
2013-12-02 17:28:01 +0100

    added more WCF support for Windows service

---
+++ XMLCreator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DAL_Library;
+
+namespace XMLCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement doc = new XElement("Documents",
+                new XElement("News",
+                    new XElement("Title","a title"),
+                    new XElement("SubTitle","my subtitle"),
+                    new XElement("Body","bla bla bla")));
+            doc.Save("C:\\xmlFileWatcherCSharp\\test.xml");
+
+            New test = new New();
+            test.Id = 5;
+            test.DepositDate = new DateTime();
+            test.SubTitle = "a test subtitle 2";
+            test.Text = "bla bla bla 2";
+            test.Title = "Title num 2";
+
+            doc = new XElement("News", test);
+            doc.Save("C:\\xmlFileWatcherCSharp\\test2.xml");
+        }
+    }
+}
