8bea65320a51ae305a4621673d40faececf2a083
miked <m.davidmike@gmail.com>
2013-12-17 22:32:12 +0100

    PhoneApp almost finished

---
+++ PhoneApp/CommentPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp.Resources;
+using PhoneApp.NewsService;
+
+namespace PhoneApp
+{
+    public partial class CommentPage : PhoneApplicationPage
+    {
+        public CommentPage()
+        {
+            InitializeComponent();
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            Commentary c = new Commentary();
+            c.Content = commentTb.Text;
+            c.Rating = (int) ratingSlider.Value;
+            People people = (People) PhoneApplicationService.Current.State["loggedUser"];
+            Paper paper = (Paper) PhoneApplicationService.Current.State["selectedPaper"];
+            MainPage.proxy.sendCommentAsync(c, people, paper);
+            MessageBox.Show("Message sent");
+        }
+    }
+}
---
+++ PhoneApp/articlesPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp.Resources;
+using PhoneApp.NewsService;
+
+namespace PhoneApp
+{
+    public partial class articlesPage : PhoneApplicationPage
+    {
+        ObservableCollection<Paper> listPapers;
+        public articlesPage()
+        {
+            InitializeComponent();
+            MainPage.proxy.getPapersCompleted += new EventHandler<NewsService.getPapersCompletedEventArgs>(getPapersCallback);
+            MainPage.proxy.getPapersAsync();
+            
+        }
+
+        public void getPapersCallback(Object sender, NewsService.getPapersCompletedEventArgs e) {
+            listPapers = e.Result;
+            papersListBox.ItemsSource = listPapers;
+        }
+
+        private void commentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (papersListBox.SelectedItem != null)
+            {
+                PhoneApplicationService.Current.State["selectedPaper"] = (Paper) papersListBox.SelectedItem;
+                NavigationService.Navigate(new Uri("/CommentPage.xaml", UriKind.Relative));
+            }
+            else
+            {
+                MessageBox.Show("Please select an article");
+            }
+        }
+    }
+}
